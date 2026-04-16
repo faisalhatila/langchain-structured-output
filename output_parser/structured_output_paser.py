f3fdab519346d1bae61c5fcec9be66d06d12fba6
Faisal Hanif <hatilas1994@gmail.com>
2026-04-16 16:52:50 +0500

    Update structured output parser to use new Hugging Face model
    - Changed the model repository from `HuggingFaceH4/zephyr-7b-beta` to `MiniMaxAI/MiniMax-M2.7` for improved text generation capabilities.
    - Updated the task type from "conversational" to "text-generation" to align with the new model's functionality.
    - Adjusted import path for `StructuredOutputParser` and `ResponseSchema` to reflect the new library structure.

--- output_parser/structured_output_paser.py
+++ output_parser/structured_output_paser.py
@@ -1,15 +1,15 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
-from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+from langchain_classic.output_parsers import StructuredOutputParser, ResponseSchema
 
 
 load_dotenv()
 
 
 llm = HuggingFaceEndpoint(
-    repo_id="HuggingFaceH4/zephyr-7b-beta",
-    task="conversational"
+    repo_id="MiniMaxAI/MiniMax-M2.7",
+    task="text-generation"
 )
 
 model = ChatHuggingFace(llm = llm)
