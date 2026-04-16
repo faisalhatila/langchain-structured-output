138945fc21dbd027b2bcb065c3778d54572550a8
Faisal Hanif <hatilas1994@gmail.com>
2026-04-16 15:31:33 +0500

    Add structured output example for review data using Pydantic with detailed descriptions
    - Introduced a new file `with_structured_output_pydantic.py` that defines a `Review` model using Pydantic, capturing key themes, summary, sentiment, pros, and cons.
    - Implemented a sample invocation of the model with a review, demonstrating structured output and environment variable usage.
    - Enhanced schema with detailed field descriptions for better understanding and usability.

---
+++ output_parser/pydantic_output_parser.py
@@ -0,0 +1,50 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+# llm = HuggingFaceEndpoint(
+#     repo_id="HuggingFaceH4/zephyr-7b-beta",
+#     task="conversational"
+# )
+llm = HuggingFaceEndpoint(
+    repo_id="MiniMaxAI/MiniMax-M2.7",
+    task="text-generation"
+)
+
+
+model = ChatHuggingFace(llm = llm)
+
+class Person(BaseModel):
+
+    name: str = Field(description='Name of the person ')
+    age: int = Field(gt=18, lt=25, description='Age of the person')
+    city: str = Field(description='Name of the city the person belongs to')
+
+
+parser = PydanticOutputParser(pydantic_object=Person)
+
+
+template = PromptTemplate(
+    template= 'Generate the name, age and city of a fictional {place} person \n {format_instruction}',
+    input_variables=['place'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+# prompt = template.invoke({'place':'Paksitani'})
+
+# result = model.invoke(prompt)
+
+# final_result = parser.parse(result.content)
+
+
+# chain approach
+
+chain = template | model | parser
+
+chain_result = chain.invoke({'place':'Spanish'})
+
+print(chain_result)
